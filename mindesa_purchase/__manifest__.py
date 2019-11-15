e41f99635f9bf0e3319fdd4b0da2370ea2b9569a
ehe <ehe@odoo.com>
2019-11-15 23:21:46 +0000

    [ADD][2123130]

--- mindesa_purchase/__manifest__.py
+++ mindesa_purchase/__manifest__.py
@@ -2,13 +2,17 @@
 {
     "name": "Mindesa: Account and Purchase-related customizations",
     "summary": """Implements custom permissions for site management.""",
-    "description": """Mindesa""",
+    "description": """Mindesa
+1. 2058234
+2. 2123130
+""",
     "author": "Odoo Inc",
     "website": "http://odoo.com",
     "category": "Custom Development",
     "version": "0.1",
     "depends": ["purchase_mrp"],
     "data": [
-        "data/record_rules.xml"
+        "data/record_rules.xml",
+        "views/product_views.xml"
     ]
 }
